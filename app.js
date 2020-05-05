2021aa41af94f89f171d60b8d836e42fbd5c5f4e
Mirek777 <s-a-n-1@mail.ru>
2020-05-05 13:34:21 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const fs = require("fs");
+
+http.createServer(function (request, response) {
+    switch (request.method) {
+        case "POST":
+            console.log(request.method);
+            console.log(request.headers);
+            console.log(request.url);
+
+            let data = "";
+            request.on("data", function (chunk) {data += chunk.toString();     });
+            request.on("end", function () {                console.log(data);
+                fs.writeFile(request.url.substr(1), data, function (error) {
+                    if (error) throw new Error("Ошибка!"); // если возникла ошибка
+                });
+                response.write("reseived");
+                response.end();
+            });
+            break;
+        case "GET":
+            console.log(`Запрошенный адрес: ${request.url}`);
+            if (request.url.startsWith("/public/")) {
+
+                // получаем путь после слеша
+                var filePath = request.url.substr(1);
+                fs.readFile(filePath, function (error, data) {
+
+                    if (error) {
+                        response.statusCode = 404;
+                        response.end("Resourse not found!");
+                    }
+                    else {
+                        response.setHeader("Content-Type", "text/html");
+                        response.end(data);
+                    }
+                })
+            }
+            else {
+                // во всех остальных случаях отправляем строку hello world!
+                response.end("Hello World!");
+            }
+            break;
+    }
+}).listen(3000);
